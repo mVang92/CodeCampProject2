43fd3df93116bbc4a1d5ab038004703f41d3446a
tunjiakanbi <tunjiakanbi@gmail.com>
2018-08-23 16:45:14 -0500

    some minor tweaks to html and css

--- Project/models/example.js
+++ Project/models/example.js
@@ -1,7 +1,16 @@
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+module.exports = function (sequelize, DataTypes) {
+  var bwaters = sequelize.define("bwaters", {
+
+    firstName = DataTypes.string,
+    lastName = DataTypes.string,
+    tripType = DataTypes.string,
+    question1 = DataTypes.int,
+    question2 = DataTypes.int,
+    question3 = DataTypes.int,
+    question4 = DataTypes.int,
+    question5 = DataTypes.int,
+    Went_Trip = DataTypes.boolean
+
   });
-  return Example;
+  return bwaters;
 };
