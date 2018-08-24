0205f3b06049e9ed759e3a72a3c9424fcd752a6f
mVang <meng.vang92@yahoo.com>
2018-08-23 22:41:42 -0500

    Logging user data

--- Project/public/js/script.js
+++ Project/public/js/script.js
@@ -1,29 +1,33 @@
 var bardata = [12, 78, 45, 15, 53];
 var height = 320,
-    // width = 600,
-    width = 320,
-    // barWidth = 50,
-    barWidth = 60,
-    barOffset = 5;
+  // width = 600,
+  width = 320,
+  // barWidth = 50,
+  barWidth = 60,
+  barOffset = 5;
 
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(bardata)])
-    .range([0, height]);
+var yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(bardata)])
+  .range([0, height]);
 
-d3.select('#viz').append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .style('background', '#C9D7D6')
-    .selectAll('rect').data(bardata)
-    .enter().append('rect')
-    .style('fill', '#C61C6F')
-    .attr('width', barWidth)
-    .attr('height', function (d) {
-        return yScale(d);
-    })
-    .attr('x', function (d, i) {
-        return i * (barWidth + barOffset)
-    })
-    .attr('y', function (d) {
-        return height - yScale(d);
-    });
+d3.select("#viz")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .style("background", "#C9D7D6")
+  .selectAll("rect")
+  .data(bardata)
+  .enter()
+  .append("rect")
+  .style("fill", "#C61C6F")
+  .attr("width", barWidth)
+  .attr("height", function(d) {
+    return yScale(d);
+  })
+  .attr("x", function(d, i) {
+    return i * (barWidth + barOffset);
+  })
+  .attr("y", function(d) {
+    return height - yScale(d);
+  });
