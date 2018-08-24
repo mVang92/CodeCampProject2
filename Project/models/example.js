42d1eb631d41655955d6066178164bdd15ff27db
tunjiakanbi <tunjiakanbi@gmail.com>
2018-08-23 20:54:08 -0500

    adjustments to 5 files on my branch

--- Project/models/example.js
+++ Project/models/example.js
@@ -1,16 +1,14 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var bwaters = sequelize.define("bwaters", {
-
-    firstName = DataTypes.string,
-    lastName = DataTypes.string,
-    tripType = DataTypes.string,
-    question1 = DataTypes.int,
-    question2 = DataTypes.int,
-    question3 = DataTypes.int,
-    question4 = DataTypes.int,
-    question5 = DataTypes.int,
-    Went_Trip = DataTypes.boolean
-
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    tripType: DataTypes.STRING,
+    question1: DataTypes.INTEGER,
+    question2: DataTypes.INTEGER,
+    question3: DataTypes.INTEGER,
+    question4: DataTypes.INTEGER,
+    question5: DataTypes.INTEGER,
+    WentTrip: DataTypes.BOOLEAN
   });
   return bwaters;
 };
--- Project/routes/htmlRoutes.js
+++ Project/routes/htmlRoutes.js
@@ -3,19 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.bwaters.findAll({}).then(function(dbBwaterss) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbBwaterss
       });
     });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.bwaters.findOne({ where: { id: req.params.id } }).then(function(dbBwaters) {
       res.render("example", {
-        example: dbExample
+        example: dbBwaters
       });
     });
   });
