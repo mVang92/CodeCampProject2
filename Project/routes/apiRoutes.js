70319f992e5c762e5707c7fd9635bab4318b195a
mVang <meng.vang92@yahoo.com>
2018-08-25 10:52:29 -0500

    New Form Handlebar page

--- Project/routes/apiRoutes.js
+++ Project/routes/apiRoutes.js
@@ -1,26 +1,28 @@
+console.log("apiRoutes.js loaded.");
+
 var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
+  app.get("/api/bwaters", function(req, res) {
     db.bwaters.findAll({}).then(function(dbBwaters) {
       res.json(dbBwaters);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  app.post("/api/bwaters", function(req, res) {
     db.bwaters.create(req.body).then(function(dbBwaters) {
       res.json(dbBwaters);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.bwaters
-      .destroy({ where: { id: req.params.id } })
-      .then(function(dbBwaters) {
-        res.json(dbBwaters);
-      });
-  });
+  // // Delete an example by id
+  // app.delete("/api/bwaters/:id", function(req, res) {
+  //   db.bwaters
+  //     .destroy({ where: { id: req.params.id } })
+  //     .then(function(dbBwaters) {
+  //       res.json(dbBwaters);
+  //     });
+  // });
 };
